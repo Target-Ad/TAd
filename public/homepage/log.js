5c5672cfa194899c88ccd44530d7200c796a9308
aresyuan <s19940422@gmail.com>
2015-12-08 23:23:07 +0800

    merge two version of syuan and bear

--- public/homepage/log.js
+++ public/homepage/log.js
@@ -10,6 +10,7 @@ $(document).ready(function(){
 		$("#mypage").show();
 	}
 	else{
+	Cookies.set('login_success','failed');
     $("#log_out").hide();
 	$("#Upload").hide();
 	$("#mypage").hide();
@@ -48,8 +49,17 @@ $(document).ready(function(){
     $('#overlay').click(function(){
         $('#overlay, #regis-block').hide();
     });
+	
+	$('#overlay').click(function(){		
+		for(var i =0; i<6;i++)		
+		{		
+			$('#overlay, #content-block-img-'+i).hide();		
+			$('#overlay, #content-block-right-'+i).hide();		
+		}		
+    });
     /*ask database for initial ad to post*/
 	var appendcontent; 
+	var thebody; 
 	$.getJSON('do', {
 		page: 'homepage',
 		action: 'getInitialData',
@@ -57,8 +67,19 @@ $(document).ready(function(){
 	}, function(r){
 		console.log(r);
 		for(var i =0; i<6;i++){
-			appendcontent = "<div class=\"col-sm-4 col-lg-4 col-md-4\"><div class=\"thumbnail\"><img id=\"Ad-image-"+i+"\" src='./postAdImage/"+r.response[i].imag+".jpg'alt=\"\"/><div class=\"caption\"><div class=\"topic\"><p id = \"Ad-topic-"+i+"\">"+r.response[i].topic+"</p></div><div class=\"contents\"><p id =\"Ad-content-"+i+"\">"+r.response[i].content+"</p></div></div><button id =\""+i+"\" class = \"discard\">discard</button><button id =\""+i+"\"class = \"collect\">keep</button></div></div> "; 
+			appendcontent = "<div class=\"col-sm-4 col-lg-4 col-md-4\"><div class=\"thumbnail\"><img id=\"Ad-image-"+i+"\" src='./postAdImage/"+r.response[i].imag+".jpg'alt=\"\"/><div class=\"caption\" id=\"more_content-"+i+"\"><div class=\"topic\"><p id = \"Ad-topic-"+i+"\">"+r.response[i].topic+"</p></div><div class=\"contents\"><p id =\"Ad-content-"+i+"\">"+r.response[i].content+"</p></div></div><button id =\""+i+"\" class = \"discard\">discard</button><button id =\""+i+"\"class = \"collect\">keep</button></div></div> "; 
 			$("#box").append(appendcontent);
+			
+			thebody = "<div id=\"content-block-img-"+i+"\" class=\"contents-display\"><img id=\"Ad-image-"+i+"\" src='./postAdImage/"+r.response[i].imag+".jpg'alt=\"\"/></div><div id=\"content-block-right-"+i+"\" class=\"contents-display-right\"><div class=\"caption\"><div class=\"topic\"><p id = \"Ad-topic-"+i+"\">"+r.response[i].topic+"</p></div><div class=\"contents\"><p id =\"Ad-content-block-"+i+"\">"+r.response[i].content+"</p></div></div><div class=\"row\" style=\"padding-top:15px;\"><div class=\"col-md-12\" ><div class=\"comment-container\" ><div class=\"col-md-1\" style=\"width: 50px;\"><img src=\"images/1.png\" /></div><div class=\"col-md-10\"><b>Syuan</b></div><div class=\"col-md-10\" style=\"background-color: #fff; height: 25px;\">It's so good. I can buy what I want !</div></div></div><div class=\"col-md-12\" ><div class=\"col-md-1\" style=\"width: 40px;\"><img src=\"images/1.png\" /></div><div class=\"col-md-11\"><input type=\"text\" class=\"form-control col-md-4\" placeholder=\"please leave a message\"></div></div></div></div>";
+			$("#thebody").append(thebody);
+			
+			$("#more_content-"+i).click(function(){
+				var id =  $(this).attr('id');
+				id = id.split('-');
+				$('#overlay, #content-block-img-'+id[1]).show();
+				$('#overlay, #content-block-right-'+id[1]).show();
+
+			});
 		}
 		$(".discard").click(function(e){
 			var id = $(this).attr("id");
@@ -165,46 +186,41 @@ $(document).ready(function(){
 	});
 
 	
-	//在前端頁面處理仍有一些邏輯問題要解決 設法區別再拿到code之後重新整理得狀況
-	function google_login(){
+	//處理回傳資料格式+存入DB
+	function fb_login(){
+		console.log(Cookies.get('login_success'));
 		var pattern, code, data;
 		pattern = new RegExp("code=(.*)");
 		isGetCode = pattern.exec(location.href);
 		if (isGetCode) {
 			if(Cookies.get('login_success')!="confirm"){
-				$('#google-login').click(function(){
+				console.log('login failed');
 				$.getJSON('do', {
 					page: 'homepage',
-					action: 'getUsrData',
+					action: 'getFbUsrData',
 					code: isGetCode[1]
 				}, function(r){
 					console.log(r);
-					$(".navbar-fixed-top").append("<div class=\"ui label\" id = \"welcome-div\"><i class=\"user icon\"></i><span id=\"welcome_usr\">welcome"+r.name+"</span></div>");
+					console.log(r.name);
+					$(".navbar-fixed-top").append("<div class=\"ui label\" id = \"welcome-div\"><i class=\"user icon\"></i><span id=\"welcome_usr\">welcome  "+r.name+"</span></div>");
 					$("#welcome_usr").css("color","#006030").css("font-size","150%");
 					Cookies.set('login_success','confirm',{expires:15, path:'/'});
 					Cookies.set('name',r.name,{expires:15, path:'/'});
 					$("#Register").hide();
 					$("#log_out").show();
 					$("#log_in").hide();
-				});
+					$("#Upload").show();
 				});
 			}
 			else{
 				location.href = "http://luffy.ee.ncku.edu.tw:8451/homepage/homepage.html";
 			}
 		} else {
-			return $('#google-login').click(function(){
-				$.getJSON('do', {
-					page: 'homepage',
-					action: 'getAuthUrl', 
-				}, function(r){
-					console.log("response get from server =\n"+r)
-					location.href = r;
-				});
-			
+			return $('#fb-login').click(function(){
+			      location.href = 'https://www.facebook.com/dialog/oauth?client_id=910359615717780&redirect_uri=http://luffy.ee.ncku.edu.tw:8451/homepage/homepage.html';
 			});
 			}
-	} google_login();
+	} fb_login();
 	
 	var logic = function( currentDateTime ){
     // 'this' is jquery object datetimepicker
