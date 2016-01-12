88d8325f5d2c4b39cfbb2654e1b0604ad7bc038f
aresyuan <s19940422@gmail.com>
2016-01-12 22:16:25 +0800

    處理user介面的廣告詳細內容部分

--- public/homepage/app.js
+++ public/homepage/app.js
@@ -18,7 +18,9 @@ $("#mypage").click(function(){
 		$.getJSON('do', data, 
 		function(r){
 			for(var i=0; i<r.postAd.length; i++){
-				appendcontent = "<div class=\"col-sm-4 col-lg-4 col-md-4\"><div class=\"thumbnail\" id=\"thumbnail-"+i+"\"><div id=\"image-container-"+i+"\" class = \"Ad-image\"><img id=\"Ad-image-"+i+"\" src='./postAdImage/"+r.postAd[i].imag+".jpg'alt=\"\"/></div><div class=\"caption\" id=\"more_content-postAd-"+i+"\"><div class=\"topic\"><p id = \"Ad-topic-"+i+"\">"+r.postAd[i].topic+"</p></div><div><span id=\"clock-start-"+i+"\"></span></div><div><span id=\"clock-end-"+i+"\"></span></div><div class=\"contents\"><p id =\"Ad-content-"+i+"\">"+r.postAd[i].discription.substring(0, 20)+".... read more"+"</p></div></div></div></div> "; 
+				appendcontent = "<div id=\"postAdAll-"+i+"\">";
+				appendcontent = appendcontent+ "<div class=\"col-sm-4 col-lg-4 col-md-4\"><div class=\"thumbnail\" id=\"thumbnail-"+i+"\"><div id=\"image-container-"+i+"\" class = \"Ad-image\"><img id=\"Ad-image-"+i+"\" src='./postAdImage/"+r.postAd[i].imag+".jpg'alt=\"\"/></div><div class=\"caption\" id=\"more_content-postAd-"+i+"\"><div class=\"topic\"><p id = \"Ad-topic-"+i+"\">"+r.postAd[i].topic+"</p></div><div><span id=\"clock-start-"+i+"\"></span></div><div><span id=\"clock-end-"+i+"\"></span></div><div class=\"contents\"><p id =\"Ad-content-"+i+"\">"+r.postAd[i].discription.substring(0, 20)+".... read more"+"</p></div></div></div></div> "; 
+				appendcontent = appendcontent+ "</div>";
 				$("#user-box").append(appendcontent);
 				$("#clock-end-"+i).hide();
 				$('#clock-start-'+i).countdown(r.postAd[i].period[0].start)
@@ -53,19 +55,27 @@ $("#mypage").click(function(){
 					$(this).html("Already Expired")
 					.parent().addClass('disabled');
 				});
-				thebody = "<div id=\"content-block-postAd-img-"+i+"\" class=\"contents-display\"><img id=\"block-Ad-image-"+i+"\" src='./postAdImage/"+r.postAd[i].imag+".jpg'alt=\"\"/></div><div id=\"content-block-right-"+i+"\" class=\"contents-display-right\"><div class=\"caption\"><div class=\"topic\"><p id = \"Ad-topic-"+i+"\">"+r.postAd[i].topic+"</p></div><div class=\"contents\"><p id =\"Ad-content-block-"+i+"\">"+r.postAd[i].discription+"</p></div></div><div class=\"row\" style=\"padding-top:15px;\"><div class=\"col-md-12\" ><div class=\"comment-container\" ><div class=\"col-md-1\" style=\"width: 50px;\"><img src=\"images/1.png\" /></div><div class=\"col-md-10\"><b>Syuan</b></div><div class=\"col-md-10\" style=\"background-color: #fff; height: 25px;\">It's so good. I can buy what I want !</div></div></div><div class=\"col-md-12\" ><div class=\"col-md-1\" style=\"width: 40px;\"><img src=\"images/1.png\" /></div><div class=\"col-md-11\"><input type=\"text\" class=\"form-control col-md-4\" placeholder=\"please leave a message\"></div></div></div></div>";
+				thebody = "<div id=\"content-block-img-postAd-"+i+"\" class=\"contents-display\"><img id=\"block-Ad-image-"+i+"\" src='./postAdImage/"+r.postAd[i].imag+".jpg'alt=\"\"/></div><div id=\"content-block-right-postAd-"+i+"\" class=\"contents-display-right\"><div class=\"caption\"><div class=\"topic\"><p id = \"Ad-topic-"+i+"\">"+r.postAd[i].topic+"</p></div><div class=\"contents\"><p id =\"Ad-content-block-"+i+"\">"+r.postAd[i].discription+"</p></div></div><div class=\"row\" style=\"padding-top:15px;\"><div class=\"col-md-12\" ><div class=\"comment-container\" ><div class=\"col-md-1\" style=\"width: 50px;\"><img src=\"images/1.png\" /></div><div class=\"col-md-10\"><b>Syuan</b></div><div class=\"col-md-10\" style=\"background-color: #fff; height: 25px;\">It's so good. I can buy what I want !</div></div></div><div class=\"col-md-12\" ><div class=\"col-md-1\" style=\"width: 40px;\"><img src=\"images/1.png\" /></div><div class=\"col-md-11\"><input type=\"text\" class=\"form-control col-md-4\" placeholder=\"please leave a message\"></div></div></div></div>";
 				$("#thebody").append(thebody);
 				
 				$("#more_content-postAd-"+i).click(function(){
-					console.log("sdf");
 					var id =  $(this).attr('id');
 					id = id.split('-');
-					$('#overlay, #content-block-img-postAd-'+id[1]).show();
-					$('#overlay, #content-block-right-postAd-'+id[1]).show();
+					$('#overlay, #content-block-img-postAd-'+id[2]).show();
+					$('#overlay, #content-block-right-postAd-'+id[2]).show();
 
 				});
+				
 			}	
-			for(var i=0; i<r.keepAd.length; i++){
+			
+			$('#overlay').click(function(){		
+				for(var i =0; i<r.postAd.length;i++)		
+				{		
+					$('#overlay, #content-block-img-postAd-'+i).hide();		
+					$('#overlay, #content-block-right-postAd-'+i).hide();		
+				}		
+			});
+			/*for(var i=0; i<r.keepAd.length; i++){
 				appendcontent = "<div class=\"col-sm-4 col-lg-4 col-md-4\"><div class=\"thumbnail\" id=\"thumbnail-"+i+"\"><div id=\"image-container-"+i+"\" class = \"Ad-image\"><img id=\"Ad-image-"+i+"\" src='./postAdImage/"+r.keepAd[i].imag+".jpg'alt=\"\"/></div><div class=\"caption\" id=\"more_content-keepAd-"+i+"\"><div class=\"topic\"><p id = \"Ad-topic-"+i+"\">"+r.keepAd[i].topic+"</p></div><div><span id=\"clock-start-"+i+"\"></span></div><div><span id=\"clock-end-"+i+"\"></span></div><div class=\"contents\"><p id =\"Ad-content-"+i+"\">"+r.keepAd[i].discription.substring(0, 20)+".... read more"+"</p></div></div><button id =\""+i+"\" class = \"discard\">discard</button><button id =\""+i+"\"class = \"collect\">keep</button></div></div> "; 
 				$("#user-page-keep-ad").append(appendcontent);
 				$("#clock-end-"+i).hide();
@@ -112,22 +122,8 @@ $("#mypage").click(function(){
 					$('#overlay, #content-block-keepAd-right-'+id[1]).show();
 
 				});
-			}	
+			}	*/
 		});
-		$('#user-page-post-ad').hide();
-		$('#user-post-ad').click(
-			function(){
-				console.log("123");
-				$('#user-page-keep-ad').hide();
-				$('#user-page-post-ad').show();
-			}
-		);
-		$('#user-collect-ad').click(
-			function(){
-				$('#user-page-keep-ad').show();
-				$('#user-page-post-ad').hide();
-			}
-		);
         $('#mainpage').hide();
         $('#mypage').hide();
         $('#userpage').show();
@@ -135,24 +131,33 @@ $("#mypage").click(function(){
     });	
 
 
-$("#btn_main").click(function(){		
+$("#btn_main").click(function(){
         $('#mainpage').show();
         $('#mypage').show();
         $('#userpage').hide();
         $('#btn_main').hide();
+		data={
+			page: 'homepage',
+			action: 'askForUsrInform',
+			usr_id: Cookies.get('_id') 
+		};
+		$.getJSON('do', data, 
+		function(r){
+			//delete all the Ad on the user page
+			for(var i=0; i<r.postAd.length; i++)
+			{
+				$("#postAdAll-"+i).remove();
+			}
+		});
     });	
 
 
 
 $("#li_pos").click(function(){		
-        $('#POST').show();
-        $('#COLLECTION').hide();
 
     });	
 
 $("#li_col").click(function(){		
-        $('#POST').hide();
-        $('#COLLECTION').show();
 
     });	
 
